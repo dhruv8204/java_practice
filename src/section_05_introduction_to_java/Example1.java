ed64cbc3a1ab172f284fa74c418e81e251ef1b22
dhruv8204 <127106891+dhruv8204@users.noreply.github.com>
2025-01-09 13:39:29 +0530

    This is the introduction to debugging tools in intellij idea

---
+++ src/section_05_introduction_to_java/Example1.java
@@ -0,0 +1,9 @@
+package section_05_introduction_to_java;
+
+
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.println("Hello world");
+    }
+}
---
+++ src/section_05_introduction_to_java/Example2.java
@@ -0,0 +1,19 @@
+package section_05_introduction_to_java;
+import java.lang.*;
+
+
+
+public class Example2 {
+    // public static void main(String[] args){}
+    public static void main(String... args){
+        System.out.println("Name: Dhruv Dalvi");
+        System.out.println("Email: dhruvdalvi786@gmail.com");
+        System.out.println("Mobile_No: 7378749332");
+        System.out.println("Address: Balaji Nagar ");
+        System.out.println("Hobbies: Playing cricket, Listening songs, Exploaring new place");
+        System.out.println("Programming Languages: C, Python, SQL, R \nData Visualization tools: Power Bi, Matplotlib, Seaborn");
+        System.out.println("DOB: 08-02-2004");
+        System.out.println("College: YCCE");
+    }
+
+}
